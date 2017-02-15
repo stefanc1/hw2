0e0482856e2da3061a67c2936166d1bfeaf48355
Andrew Yu <andrewy4@hpc-login-1-2.local>
2017-02-15 15:15:48 -0800

    Start with Susie. In  progress

--- part2/mandelbrot_susie.cc
+++ part2/mandelbrot_susie.cc
@@ -6,11 +6,88 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <mpi.h>
+#include "render.hh"
+
+#define WIDTH 1000
+#define HEIGHT 1000
+
+int
+mandelbrot(double x, double y) {
+	int maxit = 511;
+	double cx = x;
+	double cy = y;
+	double newx, newy;
+
+	int it = 0;
+	for (it = 0; it < maxit && (x*x + y*y) < 4; ++it) {
+		newx = x*x - y*y + cx;
+		newy = 2*x*y + cy;
+		x = newx;
+		y = newy;
+	}
+	return it;
+}
 
 int
 main (int argc, char* argv[])
 {
-  /* Lucky you, you get to write MPI code */
+	double minX = -2.1;
+	double maxX = 0.7;
+	double minY = -1.25;
+	double maxY = 1.25;
+	double t_start, t_elapsed;
+
+	int height, width;
+	if (argc == 3) {
+		height = atoi (argv[1]);
+		width = atoi (argv[2]);
+		assert (height > 0 && width > 0);
+	} else {
+		fprintf (stderr, "usage: %s <height> <width>\n", argv[0]);
+		fprintf (stderr, "where <height> and <width> are the dimensions of the image.\n");
+		return -1;
+	}
+	double it = (maxY - minY)/height;
+	double jt = (maxX - minX)/width;
+	double x, y;
+
+	int rank, np;
+	MPI_Status status;
+
+	//MPI Start
+	MPI_Init (&argc, &argv);
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* Get process id */
+	MPI_Comm_size (MPI_COMM_WORLD, &np);	/*Get number of processes*/
+	MPI_Barrier (MPI_COMM_WORLD);
+	if(rank == 0){
+		
+		t_start = MPI_Wtime();
+	}
+	int maxDataPerRow = height / np + 1;
+	int blockSize = maxDataPerRow * width;
+	int sendBuffer[blockSize];
+
+
+	//row = rank, rank + 1P, rank + 2P
+	y = minY + it * rank;
+	int maxRowsIterations = (height-rank)/np;
+	for (int i = 0; i < maxRowsIterations; ++i) {
+		x = minX;
+		for (int j = 0; j < width; ++j) {
+			sendBuffer[(i*width) + j] = mandelbrot(x,y);
+			x += jt;
+		}
+		y += (it*np);
+	}
+
+	MPI_Barrier (MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		int receiveBuffer[]
+	}
+
+
 }
 
 /* eof */
